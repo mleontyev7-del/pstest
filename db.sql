2aca5d7ad40dbe19ded90361678ce9b4cb74084d
mleontyev7-del <mleontyev7@gmail.com>
2025-12-05 10:35:28 +0200

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,2 @@
+ Create a New Repository (Repo for short):
+By now, you should’ve created a GitHub account and have Git installed. If you haven’t done that, then head over here. Once you’re done, head over to the Repositories section and click on “New”.
